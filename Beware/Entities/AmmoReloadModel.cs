8ba3d0a0a1e7f07d8aee51fb6b44d1108b1f47cb
Alexander Crowther <acrowther014@gmail.com>
2023-01-14 14:43:17 -0500

    Created new AmmoReload class so player can resupply special attack.

---
+++ Beware/Entities/AmmoReloadModel.cs
@@ -0,0 +1,18 @@
+﻿using Beware.EntityFeatures;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Beware.Entities {
+    class AmmoReloadModel : EntityModel {
+        public AmmoReloadModel(int startingHealth = 0, int startingImpactDamage = 0, Sprite sprite = null) : base(startingHealth, startingImpactDamage, sprite) {
+
+        }
+
+        public override void Hit(int damage = 1) {
+            this.IsExpired = true;
+        }
+
+
+    }
+}
